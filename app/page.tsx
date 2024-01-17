f2394b8684610afad5976f7c5e26780fbfa94944
Mayank Bansal <mayank.bansal@sapidblue.com>
2024-01-17 11:26:09 +0530

    QR CODE DOWNLOAD

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,22 @@ const MyComponent = () => {
   const baseUrl = typeof window !== 'undefined' ? window.origin : '';
   const url = `${baseUrl}/product?${queryParams}`;
 
-  console.log(url)
-  return (
+
+  const handleDownloadQR = () => {
+    const canvas = document.getElementById("downloadableQRCode") as HTMLCanvasElement;
+    if (canvas) {
+      const pngUrl = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = "QRCode.png";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
+   return (
     <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6 p-4">
       <div className="w-full md:w-1/2">
         <div className="mb-4">
@@ -80,8 +94,14 @@ const MyComponent = () => {
         </div>
       </div>
 
-      <div className="w-full md:w-1/4">
-        <QRCode value={url} />
+      <div className="w-full md:w-1/4 cursor-pointer" onClick={handleDownloadQR}>
+        <div className='flex justify-center'>
+        <QRCode 
+          value={url} 
+          id="downloadableQRCode"
+          renderAs="canvas"
+        /></div>
+        <p className="text-center mt-2 text-sm text-gray-500">Click to download</p>
       </div>
     </div>
   );
